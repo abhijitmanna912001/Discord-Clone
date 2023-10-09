85e6b003db74b6e11b746d6692b25808f65b4a90
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-10-09 23:45:28 +0530

    Socket IO Setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import { ModalProvider } from "@/components/provider/modal-provider";
-import { ThemeProvider } from "@/components/provider/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
@@ -28,8 +29,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
